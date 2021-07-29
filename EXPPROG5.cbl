9a5836661199be7994f346b942b2191b5fb5a7db
Hannah-Ashna <hannah.ashna.jacob@gmail.com>
2021-07-29 20:06:52 +0100

    Modify - Fixed formatting

--- EXPPROG5.cbl
+++ EXPPROG5.cbl
@@ -1,11 +1,13 @@
- ID DIVISION.
+       ID DIVISION.
        PROGRAM-ID. EXPPROG5.
        AUTHOR. HANNAH JACOB.
        DATE-WRITTEN. 30TH JULY 2021.
        DATE-COMPILED.
-      *
+      
+      *---------------------
        ENVIRONMENT DIVISION.
-      *
+      
+      *---------------------
        INPUT-OUTPUT SECTION.
       *
        FILE-CONTROL.
@@ -19,43 +21,44 @@
        FD FILE-INPUT BLOCK CONTAINS 0 RECORDS
            RECORDING MODE IS F.
        01 INPUT-RECORDS.
-           03 ACCOUNT-NUM       PIC 9(5).
-           03 INITIAL-VAL       PIC X(1).
-           03 SURNAME           PIC X(20).
-           03 MARKER            PIC X(1).
-           03 TRANS-AMOUNT      PIC 9(5).
-           03 TRANS-DATE        PIC 9(8).
-           03 TRANS-DETAILS     PIC X(20).
-           03 FILLER            PIC X(20).
+           03 ACCOUNT-NUM          PIC 9(5).
+           03 INITIAL-VAL          PIC X(1).
+           03 SURNAME              PIC X(20).
+           03 MARKER               PIC X(1).
+           03 TRANS-AMOUNT         PIC 9(5).
+           03 TRANS-DATE           PIC 9(8).
+           03 TRANS-DETAILS        PIC X(20).
+           03 FILLER               PIC X(20).
       
-      FD FILE-OUTPUT BLOCK CONTAINS 0 RECORDS
-          RECORDING MODE IS F.
-      01 OUTPUT-RECORDS.
-           03 ACCOUNT-NUM       PIC 9(5).
-           03 MARKER            PIC X(1).
-           03 TRANS-AMOUNT      PIC S9(5) COMP-3.
-           03 TRANS-DATE        PIC 9(8).
-           03 INITIAL-VAL       PIC X(1).
-           03 SURNAME           PIC X(20).
-           03 NOT-USED          PIC X(40) VALUE SPACES. 
+       FD FILE-OUTPUT BLOCK CONTAINS 0 RECORDS
+            RECORDING MODE IS F.
+       01 OUTPUT-RECORDS.
+           03 ACCOUNT-NUM          PIC 9(5).
+           03 MARKER               PIC X(1).
+           03 TRANS-AMOUNT         PIC S9(5) COMP-3.
+           03 TRANS-DATE           PIC 9(8).
+           03 INITIAL-VAL          PIC X(1).
+           03 SURNAME              PIC X(20).
+           03 NOT-USED             PIC X(40) VALUE SPACES. 
+        
+       WORKING-STORAGE SECTION.
+       01 WS-FILE-STATUS           PIC X(2).
+           88 FILE-OK              VALUE "00".
+           88 FILE-EOF             VALUE "01".
+           88 FILE-VALID           VALUE "00", "01".
       
-      WORKING-STORAGE SECTION.
-      01 WS-FILE-STATUS         PIC X(2).
-           88 FILE-OK           VALUE "00".
-           88 FILE-EOF          VALUE "01".
-           88 FILE-VALID        VALUE "00", "01".
-     *
-      PROCEDURE DIVISION.
-      A100-MAIN-LOGIC           SECTION.
+      *---------------------
+       PROCEDURE DIVISION.
+       A100-MAIN-LOGIC             SECTION.
            DISPLAY "STATUS - STARTING PROGRAM"
            PERFORM B100-INIT
-     * DO STUFF HERE???
+      * DO STUFF HERE???
            PERFORM T100-TERMINATE
            DISPLAY "STATUS - PROGRAM DONE"
            STOP RUN
            .
       
-      B100-INIT                 SECTION.
+       B100-INIT                   SECTION.
            OPEN INPUT   FILE-INPUT
                 OUTPUT  FILE-OUTPUT
           
@@ -68,7 +71,7 @@
            DISPLAY "STATUS - FILES OPENED"
            .
       
-      T100-TERMINATE            SECTION.
+       T100-TERMINATE              SECTION.
            CLOSE FILE-INPUT
                  FILE-OUTPUT
       
