ce9f32c5d496da2667f0dc36ce6255936b8061d4
Hannah Ashna Jacob <hannah.ashna.jacob@gmail.com>
2021-07-29 16:34:22 +0100

    Addition - Added base proc logic AND WS Section

--- EXPPROG5.cbl
+++ EXPPROG5.cbl
@@ -38,10 +38,39 @@
            03 INITIAL-VAL       PIC X(1).
            03 SURNAME           PIC X(20).
            03 NOT-USED          PIC X(40) VALUE SPACES. 
-
-           03 TRANS-DETAILS     PIC X(20).
-           03 FILLER            PIC X(20). 
+      
+      WORKING-STORAGE SECTION.
+      01 WS-FILE-STATUS         PIC X(2).
+           88 FILE-OK           VALUE "00".
+           88 FILE-EOF          VALUE "01".
+           88 FILE-VALID        VALUE "00", "01".
      *
       PROCEDURE DIVISION.
       A100-MAIN-LOGIC           SECTION.
+           DISPLAY "STATUS - STARTING PROGRAM"
+           PERFORM B100-INIT
      * DO STUFF HERE???
+           PERFORM T100-TERMINATE
+           DISPLAY "STATUS - PROGRAM DONE"
+           STOP RUN
+           .
+      
+      B100-INIT                 SECTION.
+           OPEN INPUT   FILE-INPUT
+                OUTPUT  FILE-OUTPUT
+          
+           IF FILE-OK
+               DISPLAY "STATUS - FILES OK"
+           ELSE
+               DISPLAY "ERROR - COULD NOT OPEN FILE"
+           END-IF
+      
+           DISPLAY "STATUS - FILES OPENED"
+           .
+      
+      T100-TERMINATE            SECTION.
+           CLOSE FILE-INPUT
+                 FILE-OUTPUT
+      
+           DISPLAY "STATUS - FILES CLOSED"
+           .
