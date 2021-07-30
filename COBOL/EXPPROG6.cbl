0305114bb04a0547c1ed07a88dc2e03dc7f5514e
Hannah Ashna Jacob <hannah.ashna.jacob@gmail.com>
2021-07-30 16:12:28 +0100

    Update EXPPROG6.cbl

--- COBOL/EXPPROG6.cbl
+++ COBOL/EXPPROG6.cbl
@@ -11,11 +11,11 @@
        INPUT-OUTPUT SECTION.
       *
        FILE-CONTROL.
-           SELECT GOODFILE     ASSIGN TO GOOD-FILE
+           SELECT GOODFILE     ASSIGN TO GOODIN
            FILE STATUS IS WS-GOOD-STATUS.
-           SELECT MASTERFILE   ASSIGN TO MASTER-FILE
+           SELECT MASTERFILE   ASSIGN TO MASTIN
            FILE STATUS IS WS-MASTER-STATUS.
-           SELECT OUTPUT-FILE  ASSIGN TO FILEOUT.
+           SELECT OUTPUTFILE  ASSIGN TO FILEOUT.
       *
        DATA DIVISION.
       *
@@ -42,22 +42,22 @@
            03 M-INITIAL-VAL        PIC X(1).
            03 M-SURNAME            PIC X(20).
            03 M-BALANCE            PIC S9(9) COMP-3.
-           03 M-HIST-TRANS OCCURS 5 TIMES.
-           03 M-MARKER             PIC X(1).
-           03 M-TRANS-AMOUNT       PIC S9(9) COMP-3.
-           03 M-FILLER             PIC X(35) VALUE SPACES.
+           03 M-HIST-TRANS OCCURS 5 TIMES INDEXED BY M-IDX.
+               05 M-MARKER         PIC X(1).
+               05 M-TRANS-AMOUNT   PIC S9(9) COMP-3.
+           03 M-FILLER             PIC X(11) VALUE SPACES.
 
-       FD OUTPUT-FILE BLOCK CONTAINS 0 RECORDS
+       FD OUTPUTFILE BLOCK CONTAINS 0 RECORDS
             RECORDING MODE IS F.
        01 OUTPUT-RECORD.
            03 O-ACCOUNT-NUM        PIC 9(5).
            03 O-INITIAL-VAL        PIC X(1).
            03 O-SURNAME            PIC X(20).
            03 O-BALANCE            PIC S9(9) COMP-3.
-           03 O-HIST-TRANS OCCURS 5 TIMES.
-           03 O-MARKER             PIC X(1).
-           03 O-TRANS-AMOUNT       PIC S9(9) COMP-3.
-           03 O-FILLER             PIC X(35) VALUE SPACES.
+           03 O-HIST-TRANS OCCURS 5 TIMES INDEXED BY O-IDX.
+               05 O-MARKER         PIC X(1).
+               05 O-TRANS-AMOUNT   PIC S9(9) COMP-3.
+           03 O-FILLER             PIC X(11) VALUE SPACES.
 
        WORKING-STORAGE SECTION.
        01 WS-GOOD-STATUS           PIC X(2).
@@ -70,63 +70,147 @@
            88 MASTER-EOF           VALUE "01".
            88 MASTER-VALID         VALUE "00", "01".
 
+       01 WS-MASTER                PIC X(1).
+           88 M-MATCH              VALUE "Y".
+           88 M-NO-MATCH           VALUE "N".
+
+       01 WS-GOOD                  PIC X(1).
+           88 G-MATCH              VALUE "Y".
+           88 G-NO-MATCH           VALUE "N".
+
+       01 WS-TEMP-IDX              PIC 9(1).
+
       *---------------------
        PROCEDURE DIVISION.
        A100-MAIN-LOGIC             SECTION.
            DISPLAY "STATUS - STARTING PROGRAM"
-           PERFORM B100-INIT
-           PERFORM R100-READ-FILE
-           PERFORM C100-PROCESS UNTIL FILE-EOF
+           PERFORM B100-INIT-STAGE1
+           PERFORM C100-MASTER-MATCH-PROCESS UNTIL MASTER-EOF
+
+           PERFORM B200-INIT-STAGE2
+           PERFORM C200-GOOD-MATCH-PROCESS UNTIL GOOD-EOF
+
            PERFORM T100-TERMINATE
            DISPLAY "STATUS - PROGRAM DONE"
            STOP RUN
            .
 
-       B100-INIT                   SECTION.
-           OPEN INPUT   FILE-INPUT
-                OUTPUT  FILE-OUTPUT
-
-           IF FILE-OK
-               DISPLAY "STATUS - FILES OK"
-           ELSE
-               DISPLAY "ERROR - COULD NOT OPEN FILE"
-           END-IF
-
-           DISPLAY "STATUS - FILES OPENED"
+       B100-INIT-STAGE1            SECTION.
+           OPEN INPUT   GOODFILE
+           OPEN OUTPUT  OUTPUTFILE
+           DISPLAY "STATUS - INIT STAGE 1 DONE"
            .
 
-       C100-PROCESS                SECTION.
-           PERFORM W100-WRITE-FILE
-           PERFORM R100-READ-FILE
+       B200-INIT-STAGE2            SECTION.
+           OPEN INPUT MASTERFILE
+           CLOSE GOODFILE
+           DISPLAY "STATUS - INIT STAGE 2 DONE"
            .
 
-       W100-WRITE-FILE             SECTION.
-           MOVE ACCOUNT-NUM TO OUT-ACCOUNT-NUM
-           MOVE INITIAL-VAL TO OUT-INITIAL-VAL
-           MOVE SURNAME     TO OUT-SURNAME
-           MOVE MARKER      TO OUT-MARKER
-           MOVE IN-DAY      TO OUT-DAY
-           MOVE IN-MONTH    TO OUT-MONTH
-           MOVE IN-YEAR     TO OUT-YEAR
-           MOVE SPACES      TO FILLER-OUT
+       C100-MASTER-MATCH-PROCESS   SECTION.
+           SET MASTER-OK TO TRUE
+           PERFORM R200-READ-GOOD
 
-           IF MARKER = "D"
-               MULTIPLY TRANS-AMOUNT BY -1 GIVING WS-TEMP
-               MOVE WS-TEMP      TO OUT-TRANS-AMOUNT
+           OPEN INPUT MASTERFILE
+           PERFORM D100-MASTER-MATCH-CHECK UNTIL MASTER-EOF
+
+           IF M-MATCH
+               DISPLAY "STATUS - M-MATCH"
+               PERFORM W100-WRITE-MATCH
            ELSE
-               MOVE TRANS-AMOUNT TO OUT-TRANS-AMOUNT
+               DISPLAY "STATUS - M-NO-MATCH"
+               PERFORM W200-WRITE-MASTER
            END-IF
+           CLOSE MASTERFILE
+           .
+
+       C200-GOOD-MATCH-PROCESS     SECTION.
+           SET GOOD-OK TO TRUE
+           PERFORM R100-READ-MASTER
+
+           OPEN INPUT GOODFILE
+           PERFORM D200-GOOD-MATCH-CHECK UNTIL GOOD-EOF
+
+           IF G-MATCH
+               DISPLAY "STATUS - G-MATCH"
+               PERFORM W100-WRITE-MATCH
+           ELSE
+               DISPLAY "STATUS - G-NO-MATCH"
+               PERFORM W300-WRITE-GOOD
+           END-IF
+           CLOSE GOODFILE
+           .
+
+       D100-MASTER-MATCH-CHECK     SECTION.
+           PERFORM R100-READ-MASTER
+           IF M-ACCOUNT-NUM = G-ACCOUNT-NUM
+               SET M-MATCH    TO TRUE
+           ELSE
+               SET M-NO-MATCH TO TRUE
+           END-IF
+           .
+
+       D200-GOOD-MATCH-CHECK       SECTION.
+           PERFORM R200-READ-GOOD
+           IF G-ACCOUNT-NUM = M-ACCOUNT-NUM
+               SET G-MATCH    TO TRUE
+           ELSE
+               SET G-NO-MATCH TO TRUE
+           END-IF
+           .
+
+       W100-WRITE-MATCH            SECTION.
+           MOVE M-ACCOUNT-NUM    TO O-ACCOUNT-NUM
+           MOVE M-INITIAL-VAL    TO O-INITIAL-VAL
+           MOVE M-SURNAME        TO O-SURNAME
+           MOVE 5 TO WS-TEMP-IDX
+           PERFORM VARYING M-IDX FROM 4 BY -1 UNTIL M-IDX = 1
+               SET  O-IDX TO WS-TEMP-IDX
+               MOVE M-MARKER(M-IDX)       TO O-MARKER(O-IDX)
+               MOVE M-TRANS-AMOUNT(M-IDX) TO O-TRANS-AMOUNT(O-IDX)
+               SUBTRACT 1 FROM WS-TEMP-IDX
+           END-PERFORM
+
+           MOVE G-MARKER        TO O-MARKER(1)
+           MOVE G-TRANS-AMOUNT  TO O-TRANS-AMOUNT(1)
+
+           ADD  G-TRANS-AMOUNT  TO M-BALANCE GIVING O-BALANCE
+           DISPLAY "OUTPUT RECORD: " OUTPUT-RECORD
            WRITE OUTPUT-RECORD
            .
 
-       R100-READ-FILE              SECTION.
-           READ FILE-INPUT AT END SET FILE-EOF TO TRUE
-           DISPLAY "RECORD DATA: " INPUT-RECORD
+       W200-WRITE-MASTER           SECTION.
+           MOVE MASTER-RECORD TO OUTPUT-RECORD
+           DISPLAY "OUTPUT RECORD: " OUTPUT-RECORD
+           WRITE OUTPUT-RECORD
+           .
+
+       W300-WRITE-GOOD             SECTION.
+           MOVE G-ACCOUNT-NUM    TO O-ACCOUNT-NUM
+           MOVE G-INITIAL-VAL    TO O-INITIAL-VAL
+           MOVE G-SURNAME        TO O-SURNAME
+
+           MOVE G-MARKER        TO O-MARKER(1)
+           MOVE G-TRANS-AMOUNT  TO O-TRANS-AMOUNT(1)
+
+           MOVE G-TRANS-AMOUNT  TO O-BALANCE
+           DISPLAY "OUTPUT RECORD: " OUTPUT-RECORD
+           WRITE OUTPUT-RECORD
+           .
+
+       R100-READ-MASTER            SECTION.
+           READ MASTERFILE AT END SET MASTER-EOF TO TRUE
+           DISPLAY "MASTER FILE DATA: " MASTER-RECORD
+           .
+
+       R200-READ-GOOD              SECTION.
+           READ GOODFILE AT END SET GOOD-EOF TO TRUE
+           DISPLAY "GOOD FILE DATA: " GOOD-RECORD
            .
 
        T100-TERMINATE              SECTION.
-           CLOSE FILE-INPUT
-                 FILE-OUTPUT
-
+           CLOSE GOODFILE
+                 MASTERFILE
+                 OUTPUTFILE
            DISPLAY "STATUS - FILES CLOSED"
            . 
