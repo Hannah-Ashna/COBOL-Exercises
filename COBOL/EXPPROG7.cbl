b3a068b6fac06560f94c3bb4f0995089bad46caa
Hannah Ashna Jacob <hannah.ashna.jacob@gmail.com>
2021-08-20 10:43:31 +0100

    Fixed looping error

--- COBOL/EXPPROG7.cbl
+++ COBOL/EXPPROG7.cbl
@@ -32,14 +32,14 @@
            03 INITIAL-VAL          PIC X(1).
            03 SURNAME              PIC X(20).
            03 BALANCE              PIC S9(9) COMP-3.
-           03 HIST-TRANS OCCURS 5 TIMES INDEXED BY M-IDX.
+           03 HIST-TRANS OCCURS 5 TIMES.
                05 MARKER           PIC X(1).
                05 TRANS-AMOUNT     PIC S9(9) COMP-3.
            03 FILLER               PIC X(19) VALUE SPACES.
 
        FD PRINTFILE BLOCK CONTAINS 0 RECORDS
             RECORDING MODE IS F.
-       01 PRINTLINE                PIC X(100).
+       01 PRINTLINE                PIC X(132).
 
        WORKING-STORAGE SECTION.
        01 WS-MAST-STATUS           PIC X(2).
@@ -52,53 +52,59 @@
            88 R-EOF                VALUE "01".
            88 R-VALID              VALUE "00", "01".
 
+       01 M-IDX                    PIC 9(1).
+
        01 WS-LINE-COUNT            PIC 9(2).
 
-       01 WS-PAGE-COUNT            PIC 9(2).
-
        01 REPORT-HEADER.
-           03 FILLER               PIC X(47) VALUE SPACES.
-           03 REPORT-TITLE         PIC X(6)  VALUE "REPORT".
-           03 FILLER               PIC X(47) VALUE SPACES.
+           03 FILLER               PIC X(63) VALUE SPACES.
+           03 FILLER               PIC X(6)  VALUE "REPORT".
+           03 FILLER               PIC X(4)  VALUE SPACES.
+           03 PAGE-COUNT           PIC 9(2).
+           03 FILLER               PIC X(57) VALUE SPACES.
 
        01 REPORT-COL-1.
-           03 ACCOUNT-COL          PIC X(7)  VALUE "ACCOUNT".
+           03 FILLER               PIC X(7)  VALUE "ACCOUNT".
            03 FILLER               PIC X(2)  VALUE SPACES.
-           03 INITIAL-COL          PIC X(7)  VALUE "INITIAL".
+           03 FILLER               PIC X(7)  VALUE "INITIAL".
            03 FILLER               PIC X(2)  VALUE SPACES.
-           03 SURNAME-COL          PIC X(11) VALUE "SURNAME".
+           03 FILLER               PIC X(11) VALUE "SURNAME".
            03 FILLER               PIC X(2)  VALUE SPACES.
-           03 BALANCE-COL          PIC X(7)  VALUE "BALANCE".
+           03 FILLER               PIC X(7)  VALUE "BALANCE".
            03 FILLER               PIC X(2)  VALUE SPACES.
            03 HIST-COL OCCURS 5 TIMES.
-               05 MARKER-COL       PIC X(3)  VALUE "C/D".
+               05 FILLER           PIC X(3)  VALUE "C/D".
                05 FILLER           PIC X(1)  VALUE SPACES.
-               05 AMOUNT-COL       PIC X(6)  VALUE "TRANS ".
+               05 FILLER           PIC X(6)  VALUE "TRANS ".
                05 FILLER           PIC X(2)  VALUE SPACES.
+           03 FILLER               PIC X(32) VALUE SPACES.
 
        01 REPORT-COL-2.
-           03 ACCOUNT-COL-2        PIC X(7)  VALUE "NUMBER ".
+           03 FILLER               PIC X(7)  VALUE "NUMBER ".
            03 FILLER               PIC X(33) VALUE SPACES.
            03 HIST-COL-2 OCCURS 5 TIMES.
-               05 MARKER-COL-2     PIC X(3)  VALUE "MKR".
+               05 FILLER           PIC X(3)  VALUE "MKR".
                05 FILLER           PIC X(1)  VALUE SPACES.
-               05 AMOUNT-COL-2     PIC X(6)  VALUE "AMOUNT".
+               05 FILLER           PIC X(6)  VALUE "AMOUNT".
                05 FILLER           PIC X(2)  VALUE SPACES.
+           03 FILLER               PIC X(32) VALUE SPACES.
 
        01 REPORT-RECORD.
-           03 ACCOUNT-OUT          PIC X(5).
-           03 FILLER               PIC X(6)  VALUE SPACES.
-           03 INITIAL-OUT          PIC X(1).
-           03 FILLER               PIC X(6)  VALUE SPACES.
-           03 SURNAME-OUT          PIC X(12).
-           03 FILLER               PIC X(1)  VALUE SPACES.
-           03 BALANCE-OUT          PIC ++(9)9.
-           03 FILLER               PIC X(2)  VALUE SPACES.
-           03 HIST-TRANS OCCURS 5 TIMES INDEXED BY R-IDX.
+           03 FILLER              PIC X VALUE SPACES.
+           03 ACCOUNT-OUT         PIC X(5).
+           03 FILLER              PIC X(5) VALUE SPACES.
+           03 INITIAL-OUT         PIC X(1).
+           03 FILLER              PIC X(6) VALUE SPACES.
+           03 SURNAME-OUT         PIC X(13).
+           03 FILLER              PIC X(1) VALUE SPACES.
+           03 BALANCE-OUT         PIC -(5)9(1).
+           03 FILLER              PIC X(3) VALUE SPACES.
+           03 HISTPRINT OCCURS 5 TIMES.
                05 MARKER-OUT       PIC X(1).
-               05 FILLER           PIC X(8)  VALUE SPACES.
-               05 TRANS-AMOUNT-OUT PIC ++(9)9.
-               05 FILLER           PIC X(16) VALUE SPACES.
+               05 FILLER           PIC X(1).
+               05 TRANS-AMOUNT-OUT PIC -(6)9(1).
+               05 FILLER           PIC X(3) VALUE SPACES.
+           03 FILLER               PIC X(26) VALUE SPACES.
       *---------------------
        PROCEDURE DIVISION.
        A100-MAIN-LOGIC             SECTION.
@@ -113,15 +119,11 @@
        B100-INIT-STAGE             SECTION.
            OPEN INPUT   MASTERFILE
            OPEN OUTPUT  PRINTFILE
+           MOVE  1      TO PAGE-COUNT
+           MOVE  5      TO WS-LINE-COUNT
            WRITE PRINTLINE FROM REPORT-HEADER AFTER ADVANCING PAGE
            WRITE PRINTLINE FROM REPORT-COL-1  AFTER ADVANCING 1 LINE
            WRITE PRINTLINE FROM REPORT-COL-2  AFTER ADVANCING 2 LINE
-           MOVE  1      TO WS-PAGE-COUNT
-           MOVE  5      TO WS-LINE-COUNT
-           MOVE 'A0001' TO ACCOUNT-NUM
-           START MASTERFILE
-                 KEY GREATER THAN OR EQUAL TO ACCOUNT-NUM
-           END-START
            DISPLAY "STATUS - INIT STAGE DONE"
            .
 
@@ -138,15 +140,15 @@
            IF WS-LINE-COUNT < 54
                ADD 2 TO WS-LINE-COUNT
                WRITE PRINTLINE FROM REPORT-RECORD
-               AFTER ADVANCING 1 LINE
+               AFTER ADVANCING 2 LINE
            ELSE
                MOVE  0 TO WS-LINE-COUNT
                ADD   7 TO WS-LINE-COUNT
-               ADD   1 TO WS-PAGE-COUNT
+               ADD   1 TO PAGE-COUNT
                WRITE PRINTLINE FROM REPORT-HEADER AFTER ADVANCING PAGE
                WRITE PRINTLINE FROM REPORT-COL-1  AFTER ADVANCING 1 LINE
                WRITE PRINTLINE FROM REPORT-COL-2  AFTER ADVANCING 2 LINE
-               WRITE PRINTLINE FROM REPORT-RECORD AFTER ADVANCING 1 LINE
+               WRITE PRINTLINE FROM REPORT-RECORD AFTER ADVANCING 2 LINE
            END-IF
            .
 
